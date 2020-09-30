647db20413fc46cb9e548633d2ebce1700ba7c67
Radu-Grigorovici <radu.grigorovici@anais.digital>
2020-09-30 18:31:15 +0300

    get tasks first try

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { getTasks } from './get-tasks/get-tasks';
 import { sum } from './sum/sum';
 
 export const Greeter = (name: string): string => `Hello ${name}`;
 
 export const Sum = sum;
+
+export const GetTasks = getTasks;
